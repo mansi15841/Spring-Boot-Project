de67081228b5e307eb78da8a88e381cb1af342bb
mansi15841 <mansidhiman966@gmail.com>
2026-09-13 00:12:43 +0530

    update

--- src/main/java/com/learning/boot/dao/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.learning.boot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learning.boot.model.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-	
-}
